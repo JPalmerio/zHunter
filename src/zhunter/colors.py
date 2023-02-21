438324ba3d4d85476ead8e3518f2d855fedd5c3d
Jesse Palmerio <jesse.palmerio@obspm.fr>
2023-02-21 17:51:26 +0100

    Reorganized colors and compressed an example file

--- src/zhunter/colors.py
+++ src/zhunter/colors.py
@@ -5,6 +5,33 @@ from zhunter import DIRS
 
 log = logging.getLogger(__name__)
 
+ABSORBER_COLORS = [
+    "#a6cee3",
+    "#1f78b4",
+    "#b2df8a",
+    "#33a02c",
+    "#fb9a99",
+    "#e31a1c",
+    "#fdbf6f",
+    "#ff7f00",
+    "#cab2d6",
+    "#6a3d9a",
+    "#ffff99",
+    "#b15928",
+]
+ABSORBER_COLORS = [
+    "#4A9EBC",  # Lightblue
+    "#B52EB0",  # Fuschia
+    "#C95D38",  # Orange
+    "#ECCA54",  # Yellow
+    "#92D754",  # Green
+    "#BC271B",  # Rust
+    "#66CBA0",  # Teal
+    "#C72A70",  # Pink
+    "#2D67EE",  # Blue
+    "#8218BB",  # Purple
+]
+
 
 def get_gradient(color):
     """Summary
