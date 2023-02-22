54ffe72abcb09dbcfba5f998c85ac9db967d65ea
Jesse Palmerio <jesse.palmerio@obspm.fr>
2023-02-22 17:40:17 +0100

    Reorganized structure of data files

--- src/zhunter/__init__.py
+++ src/zhunter/__init__.py
@@ -6,5 +6,5 @@ __version__ = "0.9.6"
 log = logging.getLogger(__name__)
 
 ROOT_DIR = Path(__file__).parents[0]
-DIRS = {"ROOT": ROOT_DIR, "UI": ROOT_DIR / "ui", "LINE": ROOT_DIR / "lines"}
+DIRS = {"ROOT": ROOT_DIR, "UI": ROOT_DIR / "ui", "DATA": ROOT_DIR / "data"}
 log.debug("DIRS: {}".format(DIRS))
