e180f7e73c8626562399beaa645681503ae66dd4
Jesse Palmerio <jesse.palmerio@obspm.fr>
2023-03-22 17:08:35 +0100

    Added some entrypoints for debugging

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@ extensions = [
     "sphinx.ext.duration",
     "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
 ]
