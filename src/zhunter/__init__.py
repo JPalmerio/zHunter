7fc3af4a0cc6a30e27a72e5af4645fb7569fade0
Jesse Palmerio <jesse.palmerio@obspm.fr>
2023-06-27 19:44:16 +0200

    Improved colors

--- src/zhunter/__init__.py
+++ src/zhunter/__init__.py
@@ -1,17 +1,5 @@
-from pathlib import Path
-import logging
-from .MainGraphicsWidget import MainGraphicsWidget
+from zhunter.MainGraphicsWidget import MainGraphicsWidget
 
 __version__ = "0.10.4"
 
-log = logging.getLogger(__name__)
-
 __all__ = ["MainGraphicsWidget"]
-
-ROOT_DIR = Path(__file__).parents[0]
-DIRS = {
-    "ROOT": ROOT_DIR,
-    "UI": ROOT_DIR / "ui",
-    "DATA": ROOT_DIR / "data",
-    "CONFIG": ROOT_DIR / "config",
-}
